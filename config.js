03cafb82b71df728361d89ed3b42033dd6399b8e
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-04-27 10:43:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,57 +3,44 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 process.env.MODE = 'public'
-process.env.antidelete = 'true'
-process.env.DATABASE_URL = 'mongodb+srv://midsoune10:midsoune7@cluster0.efae1fu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+process.env.antidelete = 'false'
+process.env.DATABASE_URL = '';
 process.env.PREFIX = '.' 
-process.env.SESSION_ID = 'Midsoune@SeaboardLiquids'
+process.env.SESSION_ID = ''
 
-global.owner = [['212621124079', '👑 𝙈𝙞𝙙𝙨𝙤𝙪𝙣𝙚 👑', true]] 
-global.baileys = '@whiskeysockets/baileys'
- global.botNumber = '212621124079' 
-  global.prems = ['212621124079', '']
-  global.mods = ['212697118528','212697118528']
-  global.lolkeysapi = 'gatadios'
-  global.installationId = ['a1i0G--iafgpxFY-JF2V-SASY_-JcirG7j6TRI8UEQ0d-WNrTbNeNRYSg7-ZGF41']
-  global.xyro = 'p3m8UTEawQ'
+ global.owner = [['رقمك', '👑 ᴍɪᴅ-ʙᴏᴛ 👑', true]] 
+ global.baileys = '@whiskeysockets/baileys'
+ global.botNumber = 'رقمك' 
+ global.prems = ['رقمك', 'رقمك']
+ global.mods = ['رقمك','رقمك']
+ global.lolkeysapi = 'gatadios'
+ global.installationId = ['TruecallerID']
+ global.xyro = 'p3m8UTEawQ'
  global.herapi = 'fee7b0be8faf'
- global.APIs = { // API Prefix 
+
+ global.APIs = { 
   nrtm: 'https://fg-nrtm.ddns.net',
   fgmods: 'https://api.fgmods.xyz',
-  xteam: 'https://api.xteam.xyz', 
-  dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
   neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  bg: 'http://bochil.ddns.net',
-  botcahx: 'https://api.botcahx.biz.id',
-  ibeng: 'https://api.ibeng.tech/docs'	
-}
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
+  akuari: 'https://api.akuari.my.id'
+ }
+
+global.APIKeys = { 
   'https://api.fgmods.xyz': 'dEBWvxCY',
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': 'gatadios',	
-  'https://violetics.pw': 'beta',
-  'https://api-fgmods.ddns.net': 'fg-dylux',
-  'https://api.botcahx.biz.id': 'Admin',
-  'https://api.ibeng.tech/docs': 'tamvan'	
+  'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
-// Sticker WM
-global.packname = '𝙈𝙞𝙙𝙨𝙤𝙪𝙣𝙚'
+  global.packname = '𝙈𝙞𝙙𝙨𝙤𝙪𝙣𝙚'
   global.author = '♛ 𝙈𝙞𝙙𝙨𝙤𝙪𝙣𝙚 ♛'
- global.wm = 'MidSoune'
+  global.wm = '◆──ᴍɪᴅ-ʙᴏᴛ──◆'
   global.igfg = '♛ 𝙈𝙞𝙙𝙨𝙤𝙪𝙣𝙚 ♛'
-  global.wait = ''
+  global.wait = '◆──ᴍɪᴅ-ʙᴏᴛ──◆'
   global.eror = '*خطأ في جلب البيانات*'
-  global.site = '•'
-global.vs = '2.0.24'
-global.botName = '𝙈𝙞𝙙𝘽𝙤𝙩'
-global.premium = 'true'  
+  global.vs = '2.0.24'
+  global.botName = '◆──ᴍɪᴅ-ʙᴏᴛ──◆'
+  global.premium = 'true'  
 
 global.rwait = '⏳'
 global.dmoji = '🤭'
